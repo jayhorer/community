56d0ae9368c6443d71e1e181ec7f6aa241724cdc
jayhorer <570846332@qq.com>
2020-10-10 15:20:27 +0800

    add chack cookies

--- src/main/java/life/majiang/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package life.majiang.community.mapper;
 import life.majiang.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * Created by Jay on 20/10/9 - 16:59
@@ -14,5 +16,6 @@ public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
      void insert(User user);
 
-
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
