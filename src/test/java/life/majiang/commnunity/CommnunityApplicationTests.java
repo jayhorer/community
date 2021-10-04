8271ceadf01abe014f091606914dc75389b692e6
jayhorer <570846332@qq.com>
2021-10-04 10:01:26 +0800

    init repo

---
+++ src/test/java/life/majiang/commnunity/CommnunityApplicationTests.java
@@ -0,0 +1,13 @@
+package life.majiang.commnunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommnunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
